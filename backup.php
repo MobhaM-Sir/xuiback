99cc62a51af980fa0953d8358abe2fab55072283
MobhaM-Sir <131079363+MobhaM-Sir@users.noreply.github.com>
2023-04-18 04:30:12 +0330

    Update backup.php

--- backup.php
+++ backup.php
@@ -1,3 +1,4 @@
+<?php
 define('API_KEY','5630492156:AAHQLx_Sp-oGGkVDIiOs4G5vOH48-vhHTBk');
 define('chat_id','-1001915433579');
 define('domain',gethostname());
@@ -15,3 +16,4 @@ function makeCURL($method , $args=[]){
     return $result;
 }
 makeCURL('sendDocument',['chat_id'=>chat_id,'document'=>new \CURLFile(__DIR__.'/x-ui-english.db', 'text/plain', __DIR__.'/x-ui-english.db'),'caption'=>domain.PHP_EOL.PHP_EOL.date('Y/m/d H:i:s')]);
+?>
